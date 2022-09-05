cc34324556b50decf049131ac963956fe9850f26
ogola5 <ogolaevance5@gmail.com>
2022-09-06 00:22:45 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,49 +1,83 @@
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
-document.addEventListener("DOMContentLoaded",  () => {
-  fetchData();
-  fetchBreed();
-  fetchSelected()
+console.log('%c HI', 'color: firebrick')
+
+const dogImages = document.querySelector("#dog-image-container");
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"; 
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'; 
+const breedsList = document.querySelector("#dog-breeds");
+const breedDropDown = document.querySelector("#breed-dropdown");
+let dogBreeds;
+
+document.addEventListener("DOMContentLoaded", () => {
+	fetchImages();
+	fetchBreeds();
+	breedsList.addEventListener('click', handleClick)
+	breedDropDown.addEventListener('change', handleChange)
 });
 
-const fetchData = () => {
-  const imgDiv = document.querySelector("#dog-image-container");
-  fetch(imgUrl)
-    .then((resp) => resp.json())
-    .then((data) => {
-      data.message.forEach((data) => {
-        imgDiv.innerHTML += `
-         <img src='${data}' alt="">
-        `;
-      });
-     
-    });
-};
-const breedUrl = "https://dog.ceo/api/breeds/list/all";
-const fetchBreed = () => {
-  const ul = document.querySelector("#dog-breeds");
-  fetch(breedUrl)
-    .then((resp) => resp.json())
-    .then((data) => {
-      const breeds = Object.keys(data.message);
-      console.log(breeds);
-      breeds.forEach((data) => {
-        ul.innerHTML += `
-        <li id='breed'>${data}</li>
-        `;
-      });
-      const item = ul.querySelectorAll("#breed");
-      item.forEach((ite) => {
-        ite.addEventListener("click", function (e) {
-          e.target.style.color = "red";
-        });
-      });
-    });
- 
-};
+// Fetches the images using the url provided. 
+let fetchImages = () => {
+	fetch(imgUrl)
+	.then(resp => resp.json())
+	.then(images => {
+		const imgs = images["message"]
+		const imgsArray = createImgElement(imgs)
+		renderImgs(imgsArray)
+	})
+}
 
-const fetchSelected = ()=>{
-    const drop = document.querySelector("#breed-dropdown");
-    drop.addEventListener("click", function (e) {
-      console.log(e.target.value);
-    });
+// Creates img tag/element and adds the image URL to the src
+let createImgElement = (imgs) => {
+	return imgs.map((image) => {
+		return `<img src="${image}">`
+	})
+}
+
+// Renders image element for each retrieved image
+let renderImgs = (imgsArray) => {
+	imgsArray.forEach(element => {
+		dogImages.innerHTML += element
+	})
+}
+
+// Fetches the dog breeds using the url provided. 
+let fetchBreeds = () => {
+	fetch(breedUrl)
+	.then(resp => resp.json())
+	.then(breeds => {
+		dogBreeds = Object.keys(breeds["message"])
+		breedsLis = createLiElement(dogBreeds)
+		renderList(breedsLis)
+	})
+}
+
+// Creates the list element for list of breeds retrieved
+let createLiElement = (dogBreeds) => {
+	return dogBreeds.map((breed) => {
+		return `<li>${breed}</li>`
+	})
+}
+
+//  Renders every dog breed in the list element created by createLiElement()
+let renderList = (breedsLis) => {
+	breedsLis.forEach((breed) => {
+		breedsList.innerHTML += breed
+	})
+}
+
+// Event Listener that changes the font color of a particular <li> on click. 
+let handleClick = (e) => {
+	if (e.target.nodeName === 'LI'){
+		if (e.target.style.color === "blue"){
+			e.target.style.color = "black"
+		} else {e.target.style.color = "blue"} 
+	}
+}
+
+// Filters breeds that start with a particular letter using a dropdown.
+let handleChange = (e) => {
+	const letter = e.target.value
+	const filteredBreeds = dogBreeds.filter(breed => breed.startsWith(letter))
+	const filteredBreedsLis = createLiElement(filteredBreeds)
+	breedsList.innerHTML = ''
+	renderList(filteredBreedsLis)
 }
